afff6ac35a0c933941a115f90fb9940c096e256a
mohan3307 <mohanrajm03032007@gmail.com>
2026-08-04 14:36:18 +0530

    Update API baseURL for production

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.PROD ? 'https://automated-student-attendance-monitoring-kbmz.onrender.com/api' : '/api',
 });
 
 api.interceptors.request.use((config) => {
